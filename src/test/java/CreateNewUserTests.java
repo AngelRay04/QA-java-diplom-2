dc572ebf8b1e4965bcb2ac82f45cfc684b287cc6
Raisa Molokova <psraisaih@yandex.ru>
2022-07-13 12:25:10 +0300

    Тест CreateNewUserTests

---
+++ src/test/java/CreateNewUserTests.java
@@ -0,0 +1,43 @@
+import io.qameta.allure.Description;
+import io.restassured.response.Response;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.assertEquals;
+
+@RequiredArgsConstructor
+@RunWith(Parameterized.class)
+public class CreateNewUserTests {
+    private UserClient userClient;
+    private final User user;
+    private final int statusCode;
+    private final String message;
+
+    @Before
+    public void setUp() {
+        userClient = new UserClient();
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getUserData() {
+        return new Object[][]{
+                {User.getRandom(), 200, null},
+                {User.getWithEmailAndPassword(), 403, "Email, password and name are required fields"},
+                {User.getWithEmailAndName(), 403, "Email, password and name are required fields"},
+                {User.getWithNameAndPassword(), 403, "Email, password and name are required fields"}
+        };
+    }
+
+    @Test
+    @Description("Создание нового пользователя")
+    public void createNewUserTest() {
+        Response response = userClient.create(user);
+        assertEquals(statusCode, response.statusCode());
+        Assert.assertEquals(message, response.path("message"));
+    }
+}
